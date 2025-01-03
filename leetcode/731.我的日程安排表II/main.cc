5192dd84b93060ed2a68a95dc59c45778525652c
zhz0817 <zhz15114585761@gmail.com>
2025-01-03 13:47:12 +0800

    有序map,差分

---
+++ leetcode/731.我的日程安排表II/main.cc
@@ -0,0 +1,392 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define FOR_INC(i, start, end) for (int i = start; i < end; ++i)
+#define FOR_DEC(i, start, end) for (int i = start; i > end; --i)
+#define FOR_INC_EQUAL(i, start, end) for (int i = start; i <= end; ++i)
+#define FOR_DEC_EQUAL(i, start, end) for (int i = start; i >= end; --i)
+// #define int long long
+
+void getNums(string s) {
+    string t;
+    for (char ch : s) {
+        if (ch == '[') {
+            t += '{';
+        } else if (ch == ']') {
+            t += '}';
+        } else if (ch == '"') {
+            t += "'";
+        } else {
+            t += ch;
+        }
+    }
+    cout << t << endl;
+}
+class Fenwick {
+public:
+    vector<int> f;
+    Fenwick(vector<int> &nums) : f(nums.size() + 1) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            add(i + 1, nums[i]);
+        }
+    }
+    static int lowBit(int n) { return n & -n; }
+    void add(int i, int val) {
+        for (; i < f.size(); i += lowBit(i)) {
+            f[i] += val;
+        }
+    }
+
+    int query(int i) {
+        int res = 0;
+        for (; i > 0; i -= lowBit(i)) {
+            res += f[i];
+        }
+        return res;
+    }
+};
+
+class UnionFind {
+    std::vector<int> root;
+    std::vector<int> rank;
+
+public:
+    explicit UnionFind(int size) {
+        root.resize(size);
+        rank.resize(size);
+        for (int i = 0; i < size; ++i) {
+            root[i] = rank[i] = i;
+        }
+    }
+
+    int find(int x) {
+        if (x == root[x]) return x;
+        return root[x] = find(root[x]);
+    }
+
+    void connect(int x, int y) {
+        if (x > y) {
+            connect(y, x);
+        }
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+        }
+    }
+
+    bool isConnected(int x, int y) { return find(x) == find(y); }
+};
+
+class KMPAutoMachine {
+private:
+    vector<vector<int>> dp;
+    string pat_;
+    int size = 128;
+
+public:
+    KMPAutoMachine(const string &pat) {
+        this->pat_ = pat;
+        int n = pat.length();
+        dp.resize(n, vector<int>(size, 0));
+        dp[0][pat[0]] = 1;
+        int pre = 0;
+        for (int j = 1; j < n; j++) {
+            for (int c = 0; c < size; c++) {
+                dp[j][c] = dp[pre][c];
+            }
+            dp[j][pat[j]] = j + 1;
+            pre = dp[pre][pat[j]];
+        }
+    }
+
+    int search(const string &txt) {
+        int M = pat_.size();
+        int N = txt.size();
+        int j = 0;
+        for (int i = 0; i < N; i++) {
+            j = dp[j][txt[i]];
+            if (j == M) {
+                return i - M + 1;
+            }
+        }
+        return -1;
+    }
+};
+
+class KMP{
+private:
+    vector<int> build_next(const string& t){
+        vector<int> next;
+        next.push_back(0);
+        int i=1;
+        int prefix_len = 0;//共同的前后缀长度
+        while(i<t.size()){
+            if(t[prefix_len] == t[i]){
+                prefix_len++;
+                next.push_back(prefix_len);
+                i++;
+            }else{
+                if(prefix_len == 0){
+                    next.push_back(0);
+                    i++;
+                }else{
+                    prefix_len = next[prefix_len-1];
+                }
+            }
+        }
+        return next;
+    }
+public:
+    int search(const string& s,const string& t){
+        vector<int> next = build_next(t);
+        int i=0,j=0;
+        while(i<s.size()){
+            if(s[i]==t[j]){
+                i++;
+                j++;
+            }else if(j>0){
+                j = next[j-1];
+            }else{
+                i++;
+            }
+            if(j==t.size()){
+                return i-j;
+            }
+        }
+        return -1;
+    }
+};
+
+struct Node {
+    Node* son[26]{};
+    Node* fail; // 当 cur.son[i] 不能匹配 target 中的某个字符时，cur.fail.son[i] 即为下一个待匹配节点（等于 root 则表示没有匹配）
+    int len; // 从根到 node 的字符串的长度，也是 node 在 trie 中的深度
+
+    Node(int len) : len(len) {}
+};
+
+struct AhoCorasick {
+    Node* root = new Node(0);
+
+    void put(string& s) { //普通的字典树
+        auto cur = root;
+        for (char b : s) {
+            b -= 'a';
+            if (cur->son[b] == nullptr) {
+                cur->son[b] = new Node(cur->len + 1);
+            }
+            cur = cur->son[b];
+        }
+    }
+
+    void build_fail() {
+        root->fail = root;
+        queue<Node*> q;
+        for (auto& son : root->son) {
+            if (son == nullptr) {
+                son = root;//方便失配时直接跳到下一个可能匹配的位置
+            } else {
+                son->fail = root; // 第一层的失配指针，都指向根节点 ∅
+                q.push(son);
+            }
+        }
+        // BFS
+        while (!q.empty()) {
+            auto cur = q.front();
+            q.pop();
+            for (int i = 0; i < 26; i++) {
+                auto& son = cur->son[i];
+                if (son == nullptr) {
+                    // 虚拟子节点 cur.son[i]，和 cur.fail.son[i] 是同一个
+                    // 方便失配时直接跳到下一个可能匹配的位置（但不一定是某个 words[k] 的最后一个字母）
+                    son = cur->fail->son[i];
+                }else{
+                    son->fail = cur->fail->son[i]; // 计算失配位置
+                    q.push(son);
+                }
+            }
+        }
+    }
+};
+
+struct Compare {
+    bool operator()(int a, int b) const {
+        return a > b; // 降序排序
+    }
+};
+
+class Solution {
+public:
+    void functor_test(){ //仿函数测试
+        set<int,Compare> set1;
+        set1.insert(1);
+        set1.insert(2);
+        set1.insert(3);
+        set1.insert(4);
+        for(auto info:set1){
+            cout<<info<<endl;
+        }
+    }
+};
+
+class SegmentTreeDynamic{
+public:
+    struct Node{
+        bool val = false, lazy = false;
+        Node *left, *right;
+        Node():val(false), lazy(false), left(nullptr), right(nullptr) {}
+    };
+private:
+    void pushUp(Node* node){
+        node->val = node->left->val+node->right->val;
+    }
+    // leftNum 和 rightNum 表示左右孩子区间的叶子节点数量
+    // 因为如果是「加减」更新操作的话，需要用懒惰标记的值✖️叶子节点的数量
+    void pushDown(Node* node,int leftNum,int rightNum){
+        if(node->left == nullptr){
+            node->left = new Node();
+        }
+        if(node->right == nullptr){
+            node->right = new Node();
+        }
+        if(node->lazy == 0){//不需要懒更新
+            return;
+        }
+        node->left->val += node->lazy*leftNum;
+        node->right->val += node->lazy*rightNum;
+        node->left->lazy += node->lazy;
+        node->right->lazy += node->lazy;
+        node->lazy = 0;
+    }
+
+    //在区间 [start, end] 中更新区间 [l, r] 的值，将区间 [l, r] ➕ val
+    void _update(Node* node,int start,int end,int l,int r,int val){
+        if (l <= start && end <= r) {
+            // 区间节点加上更新值
+            // 注意：需要✖️该子树所有叶子节点
+            node->val += (end - start + 1) * val;
+            // 添加懒惰标记
+            // 对区间进行「加减」的更新操作，懒惰标记需要累加，不能直接覆盖
+            node->lazy += val;
+            return ;
+        }
+        int mid = (start + end) >> 1;
+        // 下推标记
+        // mid - start + 1：表示左孩子区间叶子节点数量
+        // end - mid：表示右孩子区间叶子节点数量
+        pushDown(node, mid - start + 1, end - mid);
+        // [start, mid] 和 [l, r] 可能有交集，遍历左孩子区间
+        if (l <= mid) _update(node->left, start, mid, l, r, val);
+        // [mid + 1, end] 和 [l, r] 可能有交集，遍历右孩子区间
+        if (r > mid) _update(node->right, mid + 1, end, l, r, val);
+        // 向上更新
+        pushUp(node);
+    }
+
+    int _query(Node* node, int start, int end, int l, int r){
+        if (l <= start && end <= r) return node->val;
+        // 把当前区间 [start, end] 均分得到左右孩子的区间范围
+        // node 左孩子区间 [start, mid]
+        // node 左孩子区间 [mid + 1, end]
+        int mid = (start + end) >> 1, ans = 0;
+        // 下推标记
+        pushDown(node, mid - start + 1, end - mid);
+        // [start, mid] 和 [l, r] 可能有交集，遍历左孩子区间
+        if (l <= mid) ans += _query(node->left, start, mid, l, r);
+        // [mid + 1, end] 和 [l, r] 可能有交集，遍历右孩子区间
+        if (r > mid) ans += _query(node->right, mid + 1, end, l, r);
+        // ans 把左右子树的结果都累加起来了，与树的后续遍历同理
+        return ans;
+    }
+
+public:
+    const int L,R;
+    Node root;
+    SegmentTreeDynamic(int l, int r):L(l), R(r){}
+    void buildTree(Node* node,int start,int end,vector<int>& arr){ //静态建树
+        if(start == end){
+            node->val = arr[start];
+            return;
+        }
+        int mid = start+((end-start)>>1);
+        buildTree(node->left,start,mid,arr);
+        buildTree(node->right,mid+1,end,arr);
+        pushUp(node);
+    }
+    void update(int l,int r,int val){
+        _update(&root,L,R,l,r,val);
+    }
+
+    int query(int l, int r){
+        return _query(&root,L,R,l,r);
+    }
+};
+class MyCalendar {
+public:
+    SegmentTreeDynamic* st = new SegmentTreeDynamic(0,1e9);
+    MyCalendar() {
+
+    }
+
+    bool book(int startTime, int endTime) {
+        if (st->query(startTime, endTime - 1) != 0) return false;
+        // 更新该区间
+        st->update(startTime, endTime - 1, 1);
+        return true;
+    }
+};
+class MyCalendarTwo {
+public:
+    struct Compare1 { //类内结构体
+        bool operator()(const int& a, const int& b) const {//严格要求带有const，否则报错无法编译
+            return a<b;//从小到大排序
+        }
+    };
+    map<int,int,MyCalendarTwo::Compare1> treeMap;
+    MyCalendarTwo() {
+
+    }
+
+    bool book(int startTime, int endTime) {
+        int maxBook = 0;
+        treeMap[startTime]++;//初值是0，相当于上车加人
+        treeMap[endTime]--;//相当于下车减人
+        for(const auto& iter:treeMap){
+            int freq = iter.second;//出现频率
+            maxBook += freq;
+            if(maxBook>2){
+                treeMap[startTime]--;
+                treeMap[endTime]++;//还原现场
+                return false;
+            }
+        }
+        return true;
+    }
+};
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(startTime,endTime);
+ */
+
+signed main() {
+    Solution s;
+    vector<vector<int>> grid{{2,2}, {1,2}, {3,2}};
+    vector<vector<int>> grid1{{3,1}, {3,3}, {5,2}};
+    vector<int> nums{1,3};
+    vector<int> nums1{5};
+    //    std::ios::sync_with_stdio(false);
+    //    std::cin.tie(nullptr);
+    //    std::cout.tie(nullptr);
+    return 0;
+}
