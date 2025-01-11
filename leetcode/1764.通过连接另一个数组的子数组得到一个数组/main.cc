d59a571d8ada86aa91b52237efd5b19fbabc7b84
zhz0817 <zhz15114585761@gmail.com>
2025-01-11 12:22:06 +0800

    kmp、模拟

---
+++ leetcode/1764.通过连接另一个数组的子数组得到一个数组/main.cc
@@ -0,0 +1,84 @@
+template <typename T>
+class KMP {
+private:
+    std::vector<int> build_next(const std::vector<T>& t) {
+        std::vector<int> next;
+        next.push_back(0);
+        int i = 1;
+        int prefix_len = 0; // 共同的前后缀长度
+        while (i < t.size()) {
+            if (t[prefix_len] == t[i]) {
+                prefix_len++;
+                next.push_back(prefix_len);
+                i++;
+            } else {
+                if (prefix_len == 0) {
+                    next.push_back(0);
+                    i++;
+                } else {
+                    prefix_len = next[prefix_len - 1];
+                }
+            }
+        }
+        return next;
+    }
+
+public:
+    // 搜索模式串在主串中的首次出现位置
+    int search(const std::vector<T>& s, const std::vector<T>& t) {
+        if (t.empty()) return 0; // 空模式串默认匹配
+        std::vector<int> next = build_next(t);
+        int i = 0, j = 0;
+        while (i < s.size()) {
+            if (s[i] == t[j]) {
+                i++;
+                j++;
+            } else if (j > 0) {
+                j = next[j - 1];
+            } else {
+                i++;
+            }
+            if (j == t.size()) {
+                return i - j; // 返回匹配的起始位置
+            }
+        }
+        return -1; // 未找到匹配
+    }
+
+    // 统计模式串在主串中出现的次数
+    int count(const std::vector<T>& s, const std::vector<T>& t) {
+        if (t.empty()) return s.size() + 1; // 空模式串匹配所有位置
+        std::vector<int> next = build_next(t);
+        int i = 0, j = 0;
+        int ans = 0;
+        while (i < s.size()) {
+            if (s[i] == t[j]) {
+                i++;
+                j++;
+            } else if (j > 0) {
+                j = next[j - 1];
+            } else {
+                i++;
+            }
+            if (j == t.size()) {
+                ans++;
+                j = next[j - 1]; // 继续寻找下一个匹配
+            }
+        }
+        return ans;
+    }
+};
+class Solution {
+public:
+    bool canChoose(vector<vector<int>>& groups, vector<int>& nums) {
+        KMP<int> kmp;
+        for(vector<int>& group:groups){
+            int index = kmp.search(nums,group);
+            if(index == -1){
+                return false;
+            }
+            nums = vector<int>(nums.begin()+index+group.size(),nums.end());
+        }
+        return true;
+    }
+};
