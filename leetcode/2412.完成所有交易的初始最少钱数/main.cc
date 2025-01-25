a82532ea8c0b5434af6bceb7a8dcf135e49e99ac
zhz0817 <zhz15114585761@gmail.com>
2025-01-25 09:15:10 +0800

    贪心

---
+++ leetcode/2412.完成所有交易的初始最少钱数/main.cc
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long minimumMoney(vector<vector<int>>& transactions) {
+        long long sum = 0;
+        for(vector<int>& tmp:transactions){
+            if(tmp[0]>tmp[1]){
+                sum+=(tmp[0]-tmp[1]);
+            }
+        }
+        long long ans = 0;
+        for(vector<int>& tmp:transactions){
+            if(tmp[0]>tmp[1]){
+                ans = max(ans,sum+tmp[1]);
+            }else{
+                ans = max(ans,sum+tmp[0]);
+            }
+        }
+        return ans;
+    }
+};
